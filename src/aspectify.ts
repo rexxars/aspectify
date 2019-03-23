5edee03073d56da1aa653f8b606e79cf4fc16d2f
Espen Hovlandsdal <espen@hovlandsdal.com>
2019-03-23 17:48:41 +0100

    Match help with readme

--- src/aspectify.ts
+++ src/aspectify.ts
@@ -24,14 +24,14 @@ const cli = meow(
   `
     Usage
       $ aspectify <...files>
- 
+
     Options
-      -a, --aspect       Aspect ratio to use (eg: 16:9, 16:10, 4:3, 1.77)
+      -a, --aspect       Aspect ratio to use (eg: 16:9, 4:3, 1.77) - default is 16:9
       -o, --output       Output filename (default: <filename>.<aspect>.<ext>)
       -r, --replace      Replace the original file
       -c, --concurrency  Maximum number of crops to perform simultaneously
       -v, --verbose      Be verbose about operations performed
- 
+
     Examples
       $ aspectify you.jpg
       $ aspectify -o target.png source.png
